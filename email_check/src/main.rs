01389cc9d05ac5d5d33b15072312f97185e70091
Max Dobeck <msdobeck@gmail.com>
2019-10-20 19:42:26 -0700

    Have basic working example that returns Some() or None().

--- email_check/src/main.rs
+++ email_check/src/main.rs
@@ -1,3 +1,18 @@
+extern crate regex;
+
+use regex::Regex;
+
+fn found_email(text: &str) -> Option<&str> {
+    // define the reg expression you're looking for
+    let re = Regex::new(r"[[:word:]]+@[[:word:]]+\.[[:word:]]+").unwrap();
+
+    // capture any matching expressions and
+    re.find(text).map(|email_found| email_found.as_str())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sample: &str =
+        "this is a long piece of text containing one email address: somebody@company.com";
+    let email = found_email(sample);
+    println!("{:?}", email)
 }
