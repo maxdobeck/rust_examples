ef9cd13c9bbdb3c42085948db138f9959d13ad13
Max Dobeck <msdobeck@gmail.com>
2019-10-22 23:31:47 -0700

    Added clearer stdout output.

--- email_check/src/main.rs
+++ email_check/src/main.rs
@@ -15,14 +15,15 @@ fn main() {
     let sample: &str =
         "this is a long piece of text containing one email address: ahumanbeing@someorg.horse";
     let some_email = found_email(sample);
-    println!("{}", some_email.unwrap());
-    println!("{:?}", some_email);
+    println!("//unwrapping the Some() return vs No unwrap");
+    print!("{} vs {:?}\n\n", some_email.unwrap(), some_email);
 
-    // Printing {:?} Some or None objects vs Panicing while trying to .unwrap()
+    // Printing {:?} Some or None objects vs Panicking while trying to .unwrap()
     let sample_missing_email: &str =
         "this is a long piece of text containing one email address: whoops-Email-has-been-corrupted!";
     let missing_email = found_email(sample_missing_email);
-    println!("{:?}", missing_email);
+    println!("// Printing Some or None objects vs Panicking while trying to .unwrap()");
+    println!("{:?}\n", missing_email);
     // this will Panic!
     //println!("{}", missing_email.unwrap());
 
@@ -30,21 +31,24 @@ fn main() {
     let sample: &str =
         "this is a long piece of text containing one email address: somebody@company.com";
     let email = found_email(sample);
+    println!("// check if value is_some() and print it");
     if email.is_some() {
-        println!("{}", email.unwrap());
+        println!("{}\n", email.unwrap());
     }
 
     // if let and print the unwrapped value
     let sample2: &str = "Reach me at aperson@somebusiness.com.";
+    println!("// if let and print the unwrapped value");
     if let Some(v) = found_email(sample2) {
-        println!("{:?}", v)
+        println!("{:?}\n", v)
     }
 
     // if let with an option for the None
     let sample3: &str = "There is no email to be found here!";
     let no_email = found_email(sample3);
+    println!("// if let with an option for the None");
     if let Some(i) = no_email {
-        println!("{:?}", i)
+        println!("{:?}\n", i)
     } else {
         println!("Could not find an email")
     }
